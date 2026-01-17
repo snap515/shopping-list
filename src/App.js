24c3bb786e461f0ed94558e88f910ab8992dff1e
apedan <apedan@arigo.de>
2026-01-17 15:07:04 +0100

    Fix web linking prefixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
 
+const baseUrl = Linking.createURL('/');
+
 const linking = {
-  prefixes: ['http://localhost:19006', 'http://localhost:8081'],
+  prefixes: [
+    baseUrl,
+    'https://snap515.github.io/shopping-list',
+    'http://localhost:19006',
+    'http://localhost:8081',
+  ],
   config: {
     screens: {
       Login: 'login',
       Register: 'register',
       Lists: 'lists',
       ListDetails: 'lists/:listId',
+      Invites: 'invites',
     },
   },
 };
